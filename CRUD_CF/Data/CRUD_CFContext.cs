b52a8e6824aac0403c922634beb4c13035083265
aaliamer608 <aaliamer608@gmail.com>
2022-04-28 07:45:11 -0400

    Add project files.

---
+++ CRUD_CF/Data/CRUD_CFContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_CF.Data
+{
+    public class CRUD_CFContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CRUD_CFContext() : base("CRUD_CFContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CRUD_CF.Models.Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeContext.cs
@@ -0,0 +1,27 @@
+﻿using CRUD_CF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_CF.DAL
+{
+    public class EmployeeContext : base("EmployeeContext")
+    {
+        public EmployeeContext()
+            : base("EmployeeContext")
+        {
+        }
+
+        public DbSet<employee> Employees { get; set; }
+        public DbSet<enrollment> Enrollments { get; set; }
+        public DbSet<department> Departments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<pluralizingtablenameconvention>();
+        }
+    }
+}
